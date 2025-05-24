6d9a205d4951f1136e4f106db94b28e85ac06960
liamhall64@gmail.com <liamhall@DESKTOP-FS12P52.>
2025-05-24 17:34:33 +0100

    Initial simulations for the solar system adding in two files: solar_system.py (which is the base code for the background and sun/planets definitions) and simple_soar_system.py (the actual code that produces the simulation)

---
+++ simple_solar_system.py
@@ -0,0 +1,17 @@
+# simple_solar_system.py
+
+from solar_system import SolarSystem, Sun, Planet
+
+solar_system = SolarSystem(width=1400, height=900)
+
+sun = Sun(solar_system, mass=10_000)
+planet = Planet(
+    solar_system,
+    mass=1,
+    position=(-350, 0),
+    velocity=(0, 1),
+)
+
+while True:
+    solar_system.accelerate_due_to_gravity(sun, planet)
+    solar_system.update_all()
